df2241c9bcb924e466d8a01cfdbf41ca82c9d8e3
Alex Benedetto <alex061994@gmail.com>
2022-02-13 11:24:02 -0800

    wip

--- asset.go
+++ asset.go
@@ -1,5 +1,31 @@
 package opensea
 
+import (
+	"context"
+	"encoding/json"
+	"log"
+)
+
+type AssetWithout struct {
+	TokenID              string         `json:"token_id"`
+	NumSales             int64          `json:"num_sales"`
+	BackgroundColor      string         `json:"background_color"`
+	ImageURL             string         `json:"image_url"`
+	ImagePreviewURL      string         `json:"image_preview_url"`
+	ImageThumbnailURL    string         `json:"image_thumbnail_url"`
+	ImageOriginalURL     string         `json:"image_original_url"`
+	AnimationURL         string         `json:"animation_url"`
+	AnimationOriginalURL string         `json:"animation_original_url"`
+	Name                 string         `json:"name"`
+	Description          string         `json:"description"`
+	ExternalLink         string         `json:"external_link"`
+	// AssetContract        *AssetContract `json:"asset_contract"`
+	Owner                *Account       `json:"owner"`
+	Permalink            string         `json:"permalink"`
+	// Collection           *Collection    `json:"collection"`
+	Decimals int64 `json:"decimals"`
+}
+
 type Asset struct {
 	TokenID              string         `json:"token_id"`
 	NumSales             int64          `json:"num_sales"`
@@ -45,18 +71,24 @@ type AssetContract struct {
 	PayoutAddress               Address     `json:"payout_address"`
 }
 
-func (o Opensea) GetListOfAssetsByAddress(address string) ([]*Asset, error) {
-	ctx = context.TODO();
+func (o Opensea) GetListOfAssetsByAddress(address string) ([]*AssetWithout, error) {
+	var ctx = context.TODO();
 	return o.GetListOfAssetsByAddressWithContext(ctx, address)
 }
 
-func (o Opensea) GetListOfAssetsByAddressWithContext(ctx context.Context, address string) (assets []*Asset, error) {
+func (o Opensea) GetListOfAssetsByAddressWithContext(ctx context.Context, address string) ([]*AssetWithout, error) {
+	// assets := []*Asset{}
 	path := "/api/v1/assets?order_direction=desc&owner=" + address
 	b, err := o.getPath(ctx, path);
 	if err != nil {
 		return nil, err;
 	}
-	assets = new([]Asset)
-	err = json.Unmarshal(b, assets)
-	return
+	out := &struct {
+		Assets	[]*AssetWithout `json:"assets"`
+	}{}
+	err = json.Unmarshal(b, out)
+	if err != nil {
+		log.Fatal(err);
+	}
+	return out.Assets, err
 }
